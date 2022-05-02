4cdc7104f47bddb12f06eb70fbc5f2d4fcaf2cd9
melek6 <melekbarhoumi58@gmail.com>
2022-05-02 18:46:51 +0100

    listeProfile

--- src/main/java/com/MissionNDF/springjwt/controllers/AuthController.java
+++ src/main/java/com/MissionNDF/springjwt/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.MissionNDF.springjwt.controllers;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,18 +8,18 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import com.MissionNDF.springjwt.models.DemandeMiss;
+import com.MissionNDF.springjwt.repository.DemandeMissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.MissionNDF.springjwt.models.ERole;
 import com.MissionNDF.springjwt.models.Role;
@@ -136,5 +137,18 @@ public class AuthController {
 
 
    // return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+  }
+
+
+
+
+  //affiche tout les mission
+  @GetMapping("/AllUser")
+  @PreAuthorize("hasRole('ROLE_COLLABORATEUR')" + " || hasRole('ROLE_MANAGER')")
+
+
+  public Collection<User> getAllUser() throws ChangeSetPersister.NotFoundException {
+
+    return userRepository.findAll();
   }
 }
